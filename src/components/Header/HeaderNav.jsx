95851050bdaf3b5a6fa223b0634df1cc429a5472
Truong Toan <toanb2110106@student.ctu.edu.vn>
2024-08-24 23:18:30 +0700

    fix: style of header component

--- src/components/Header/HeaderNav.jsx
+++ src/components/Header/HeaderNav.jsx
@@ -16,12 +16,12 @@ const HeaderNav = () => {
   return (
     <>
       {/* Navbar */}
-      <div className="relative headernav-container flex items-center justify-between py-2 bg-bgPrimary xl:px-[80px] lg:px-[20px] sm:px-[10px]">
+      <div className="relative headernav-container flex items-center justify-between py-3 bg-bgPrimary px-2 xl:px-[80px] lg:px-[20px] sm:px-[10px]">
         {/* Logo */}
         <div>
           <NavLink
             to="/"
-            className="w-[150px] h-[65px] flex items-center justify-between"
+            className="w-[120px] flex items-center justify-between sm:w-[130px] lg:w-[140px]"
           >
             <img src={logo} alt="logo" />
           </NavLink>
@@ -72,7 +72,7 @@ const HeaderNav = () => {
           </NavLink>
         </ul>
         {/* Search Cart User */}
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2.5 lg:gap-4">
           {/* Search-icons */}
           <div className="group relative">
             <span
@@ -82,7 +82,7 @@ const HeaderNav = () => {
             >
               <IoIosSearch className="text-3xl text-primary cursor-pointer hover:text-tertiary duration-500" />
             </span>
-            <div className="sm:group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="hidden sm:block absolute dropdown-menu right-0 pt-4 transition-all duration-300 will-change-transform scale-90 opacity-0 sm:invisible sm:group-hover:visible sm:group-hover:scale-100 sm:group-hover:opacity-100">
               <div className="flex flex-col gap-3 w-96 p-4 bg-primary rounded-md">
                 <p className="text-md text-secondar font-medium">
                   TÌM KIẾM MÓN ĂN CỦA BẠN:
@@ -94,15 +94,15 @@ const HeaderNav = () => {
                     <input
                       type="text"
                       placeholder="Nhập món ăn bạn muốn tìm..."
-                      className="w-full border rounded-md border-tertiary placeholder:text-secondary p-2 text-sm outline-none"
+                      className="w-full border rounded-md border-tertiary placeholder:text-secondary px-2 py-2.5 text-sm outline-none"
                     />
-                    <button className="absolute right-1 top-1 text-tertiary text-3xl">
+                    <button className="absolute right-1.5 top-1/2 -translate-y-1/2 text-tertiary text-[26px]">
                       <IoIosSearch />
                     </button>
                   </div>
                   <hr />
                   {/* Result search */}
-                  <div className="absolute mt-3 p-3 flex flex-col gap-3 w-full bg-zinc-100 rounded-lg">
+                  <div className="mt-3 p-3 flex flex-col gap-3 w-full bg-zinc-100 rounded-lg">
                     <h2 className="text-md text-secondar font-normal">
                       KẾT QUẢ TÌM KIẾM:
                     </h2>
@@ -119,7 +119,7 @@ const HeaderNav = () => {
           {/* Cart-icons */}
           <Link to="/cart/12" className="relative">
             <BsCart3 className="text-2xl text-primary cursor-pointer" />
-            <p className="absolute flex justify-center items-center rounded-full bg-tertiary text-sm text-primary aspect-square w-[20px] h-[20px] text-[10px] left-3 bottom-4">
+            <p className="absolute flex justify-center items-center rounded-full bg-tertiary text-sm text-primary aspect-square w-[20px] h-[20px] text-[10px] -top-2 -right-2.5">
               9
             </p>
           </Link>
@@ -128,23 +128,29 @@ const HeaderNav = () => {
             <span>
               <FaRegUser className="text-2xl text-primary cursor-pointer hover:text-tertiary duration-500" />
             </span>
-            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 cursor-pointer">
-              <div className="flex flex-col gap-3 w-52 p-3 bg-primary rounded-md">
-                <p className="bg-tertiary p-1 rounded-md text-center text-sm text-primary hover:bg-yellow-600 duration-500">
+            <div className="absolute dropdown-menu right-0 pt-4 cursor-pointer transition-all duration-300 will-change-transform scale-90 opacity-0 invisible group-hover:visible group-hover:scale-100 group-hover:opacity-100">
+              <div className="flex flex-col gap-3 w-60 p-3 bg-primary rounded-md">
+                <Link
+                  to="/login"
+                  className="bg-tertiary p-2.5 rounded-md text-center text-sm font-medium text-primary hover:bg-yellow-600 duration-500"
+                >
                   Đăng nhập
-                </p>
-                <p className="bg-tertiary p-1 rounded-md text-center text-sm text-primary hover:bg-yellow-600 duration-500">
+                </Link>
+                <Link
+                  to="/register"
+                  className="bg-tertiary p-2.5 rounded-md text-center text-sm font-medium text-primary hover:bg-yellow-600 duration-500"
+                >
                   Đăng ký
-                </p>
-                <p className="bg-tertiary p-1 rounded-md text-center text-sm text-primary hover:bg-yellow-600 duration-500">
+                </Link>
+                <Link className="bg-tertiary p-2.5 rounded-md text-center text-sm font-medium text-primary hover:bg-yellow-600 duration-500">
                   Món ăn yêu thích
-                </p>
+                </Link>
               </div>
             </div>
           </div>
           {/* Booking */}
           <Link className="hidden booking-table md:flex" to="/booking">
-            <button className="bg-tertiary text-primary text-sm rounded-md font-bold hover:bg-yellow-600 duration-500 w-20 h-8">
+            <button className="bg-tertiary text-primary text-sm rounded-md font-bold hover:bg-yellow-600 duration-500 py-2.5 px-2 lg:px-4">
               Đặt bàn
             </button>
           </Link>
