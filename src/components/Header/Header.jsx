066763cafe77606bfffb5e06b208991a7d8f182b
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-08-25 17:10:39 +0700

    fix: recover header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import HeaderNav from "./HeaderNav";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <HeaderNav />
+    </div>
+  );
+};
+
+export default Header;
