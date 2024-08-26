b9cce697a8853213ffd6db9102e34bd40015787a
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-08-26 22:17:17 +0700

    refactory: feat back to top

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import HeaderNav from "./HeaderNav";
-
+import BackTop from "./BackTop";
 const Header = () => {
   return (
     <div className="header sticky top-0 bg-bgPrimary">
       <HeaderNav />
+      <BackTop />
     </div>
   );
 };
