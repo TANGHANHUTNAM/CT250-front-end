d7d4b7ec63179e9079d17c6a2c1d7ebe6ffa63fa
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-08-27 20:59:32 +0700

    refactor: completed translation policy page

--- src/pages/MembershipPolicy.jsx
+++ src/pages/MembershipPolicy.jsx
@@ -1,27 +1,20 @@
 import { useDynamicTitle } from "../hooks";
-
+import { useTranslation } from "react-i18next";
 const MembershipPolicyPage = () => {
   useDynamicTitle("Chính sách thành viên");
-
+  const { t } = useTranslation();
   return (
     <div className="bg-bgPrimary">
       <div className="max-w-screen-xl mx-auto w-full text-primary px-3 flex flex-col gap-5 font-medium py-10 ">
-        <p className="text-3xl font-semibold">
-          Chính sách thành viên Điều kiện chính sách thành viên
-        </p>
-        1. Thẻ thành viên Điều kiện cấp thẻ thành viên: Khi khách hàng mua hàng
-        trên hệ thống nhà hàng Dola Restaurant sẽ được cấp thẻ thành viên.
-        <p>2. Thẻ VIP</p>
-        <p>Điều kiện nhận thẻ VIP:</p>
-        <p>+ Có giá trị tổng đơn hàng lớn hơn 15 triệu/ tháng</p>
-        <p>+ Mua hàng với giá trị 3 triệu trợ lên</p>
-        <p>+ Tham gia các hoạt động, chương trình khuyến mãi của Tonatra</p>
-        <p>
-          Lưu ý: Hạn mức 10, 20, 30, 50,100 triệu đồng là tính từ thời điểm bắt
-          đầu mua tới khi lên thẻ. Khi lên thẻ VIP và tích tiếp lên 20 đến 100
-          triệu, tổng tiền này là tính từ khi khách hàng mua lần đầu và cộng dồn
-          lên.
-        </p>
+        <p className="text-3xl font-semibold">{t("MembershipPolicy.title")}</p>
+        <p>{t("MembershipPolicy.content_1")}</p>
+        <p>{t("MembershipPolicy.content_2")}</p>
+        <p>{t("MembershipPolicy.content_3")}</p>
+        <p>{t("MembershipPolicy.content_4")}</p>
+        <p>{t("MembershipPolicy.content_5")}</p>
+        <p>{t("MembershipPolicy.content_6")}</p>
+        <p>{t("MembershipPolicy.content_7")}</p>
+        <p>{t("MembershipPolicy.content_8")}</p>
       </div>
     </div>
   );
