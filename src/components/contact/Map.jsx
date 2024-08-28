51f7f5f8ae9f8c2481e3adf1e739cbb90f1038e1
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-08-28 09:26:40 +0700

    style: UI responsive page contact

---
+++ src/components/contact/Map.jsx
@@ -0,0 +1,17 @@
+const Map = () => {
+  return (
+    <div className="map p-4" style={{ width: '100%', height: '400px' }}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.8415184420487!2d105.76804037404074!3d10.029933690077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0895a51d60719%3A0x9d76b0035f6d53d0!2zxJDhuqFpIGjhu41jIEPhuqduIFRoxqE!5e0!3m2!1svi!2s!4v1724778542060!5m2!1svi!2s"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Map;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,19 @@
 import { useDynamicTitle } from "../hooks";
 import { useTranslation } from "react-i18next";
-
+import ContactForm from "../components/contact/contactForm";
+import Map from "../components/contact/map";
 const ContactPage = () => {
   const { t } = useTranslation();
   useDynamicTitle(t("BreadcrumbsAndTitle.contact"));
 
-  return <>ContactPage</>;
+  return (
+    <div className="Contact-page bg-bgPrimary">
+      <div className="max-w-screen-xl mx-auto w-full flex flex-col py-10">
+        <ContactForm />
+        <Map />
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
