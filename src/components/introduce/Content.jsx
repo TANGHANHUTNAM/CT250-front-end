bce3be7e54169791b088a9bb970efba7157af933
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-09-01 20:28:36 +0700

    feat: completed introduce page

---
+++ src/components/introduce/Content.jsx
@@ -0,0 +1,26 @@
+const Content = () => {
+  return (
+    <div className="content-container text-primary">
+      <div className="content-title relative text-center">
+        <p className="py-4 text-4xl font-medium uppercase">
+          Giới thiệu
+          <span className="absolute bottom-0 left-0 right-0 mx-auto box-border w-[100px] border-b-[6px] border-double"></span>
+        </p>
+      </div>
+      <div className="content-main mt-3 text-sm font-medium">
+        <p>
+          TONATRA RESTAURANT - Nhà hàng ẩm thực hiện đại kết hợp với truyền
+          thống, tạo nên tính mới lạ cho thực khách. Được ra đời vào năm 2024
+          với tiêu chí <b>Khách hàng là trên hết</b> nên chúng tôi luôn tự hào
+          về cách phục vụ cũng như các món ăn mà chúng tôi làm ra. Nhà hàng
+          chúng tôi luôn luôn đặt khách hàng lên hàng đầu, tận tâm phục vụ, mang
+          lại cho khách hàng những trãi nghiệm tuyệt với nhất. Các món ăn với
+          công thức độc quyền sẽ mang lại hương vị mới mẻ cho thực khách.
+          Tonatra Restaurant xin chân thành cảm ơn.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
