bb2734fea9b27059e57080d2eaa7398a03c5849c
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-09-02 20:58:35 +0700

    feat: translate multi language page Booking

--- src/components/booking/Booking.jsx
+++ src/components/booking/Booking.jsx
@@ -3,33 +3,30 @@ import Input from "../inputs/Input";
 import InputDate from "../inputs/InputDate";
 import { useAppForm } from "../../hooks";
 import "./Booking.css";
-
+import { useTranslation } from "react-i18next";
 const Booking = () => {
+  const { t } = useTranslation();
   const bookingFormSchema = yup
     .object({
-      fullname: yup.string().required("Họ và tên không được để trống"),
+      fullname: yup.string().required(t("Booking.required_fullName")),
       email: yup
         .string()
-        .email("Email không hợp lệ")
-        .required("Email không được để trống"),
+        .email(t("Booking.invalid_email"))
+        .required(t("Booking.required_email")),
       phone: yup
         .string()
-        .matches(/^[0-9]{10}$/, "Số điện thoại không hợp lệ")
-        .required("Số điện thoại không được để trống"),
+        .matches(/^[0-9]{10}$/, t("Booking.invalid_phoneNumber"))
+        .required(t("Booking.required_phoneNumber")),
       number: yup
         .number()
         .transform((value, originalValue) =>
           originalValue === "" ? undefined : value,
         )
-        .min(1, "Số lượng người tối thiểu là 1")
-        .max(10, "Số lượng người tối đa là 10")
-        .required("Số lượng người không được để trống"),
-      arrivalDate: yup
-        .string()
-        .required("Ngày đến nhận bàn không được để trống"),
-      arrivalTime: yup
-        .string()
-        .required("Thời gian nhận bàn không được để trống"),
+        .min(1, t("Booking.invalidMinNumber"))
+        .max(10, t("Booking.invalidMaxNumber"))
+        .required(t("Booking.required_numberOfPeople")),
+      arrivalDate: yup.string().required(t("Booking.required_arrivalDate")),
+      arrivalTime: yup.string().required(t("Booking.required_arrivalTime")),
     })
     .required();
 
@@ -49,7 +46,7 @@ const Booking = () => {
       <div className="bg-image-main mx-auto flex max-w-screen-xl flex-col items-center justify-center rounded-2xl bg-booking sm:px-8 sm:py-6">
         <div className="main-booking w-full rounded-xl bg-bgPrimary p-4 md:w-3/4 lg:w-[55%]">
           <div className="booking-title py-4 text-center font-dancingscript text-3xl text-primary md:text-5xl">
-            <p>Liên hệ đặt bàn</p>
+            <p>{t("Booking.title")}</p>
           </div>
           <form
             onSubmit={handleSubmit(handleBooking)}
@@ -57,10 +54,12 @@ const Booking = () => {
           >
             <div className="flex flex-col gap-4 md:flex-row md:gap-3">
               <div className="w-full md:w-1/2">
-                <label className="font-semibold">Họ và tên</label>
+                <label className="font-semibold">
+                  {t("Booking.label_fullName")}
+                </label>
                 <Input
                   type="text"
-                  placeholder={"Nhập họ và tên"}
+                  placeholder={t("Booking.input_fullName")}
                   label="fullname"
                   autoComplete="fullname"
                   className="mt-1 w-full rounded-md border-b-2 border-tertiary px-4 py-2.5 text-secondary outline-none"
@@ -71,10 +70,12 @@ const Booking = () => {
                 />
               </div>
               <div className="w-full md:w-1/2">
-                <label className="font-semibold">Email</label>
+                <label className="font-semibold">
+                  {t("Booking.label_email")}
+                </label>
                 <Input
                   type="email"
-                  placeholder={"Nhập email"}
+                  placeholder={t("Booking.input_email")}
                   label="email"
                   autoComplete="email"
                   className="mt-1 w-full rounded-md border-b-2 border-tertiary px-4 py-2.5 text-secondary outline-none"
@@ -87,10 +88,12 @@ const Booking = () => {
             </div>
             <div className="flex flex-col gap-4 md:flex-row md:gap-3">
               <div className="w-full md:w-1/2">
-                <label className="font-semibold">Số điện thoại</label>
+                <label className="font-semibold">
+                  {t("Booking.label_phoneNumber")}
+                </label>
                 <Input
                   type="text"
-                  placeholder={"Nhập số điện thoại"}
+                  placeholder={t("Booking.input_phoneNumber")}
                   label="phone"
                   autoComplete="phone"
                   className="mt-1 w-full rounded-md border-b-2 border-tertiary px-4 py-2.5 text-secondary outline-none"
@@ -101,10 +104,12 @@ const Booking = () => {
                 />
               </div>
               <div className="w-full md:w-1/2">
-                <label className="font-semibold">Số lượng người</label>
+                <label className="font-semibold">
+                  {t("Booking.label_number")}
+                </label>
                 <Input
                   type="number"
-                  placeholder={"Nhập số lượng"}
+                  placeholder={t("Booking.input_number")}
                   label="number"
                   autoComplete="number"
                   className="mt-1 w-full rounded-md border-b-2 border-tertiary px-4 py-2.5 text-secondary outline-none"
@@ -119,7 +124,9 @@ const Booking = () => {
             </div>
             <div className="flex flex-col gap-4 md:flex-row md:gap-3">
               <div className="w-full md:w-1/2">
-                <label className="font-semibold">Ngày đến nhận bàn</label>
+                <label className="font-semibold">
+                  {t("Booking.label_arrivalDate")}
+                </label>
                 <InputDate
                   label="arrivalDate"
                   className="mt-1 w-full rounded-md border-0 !border-b-2 border-tertiary px-4 py-2.5 text-secondary outline-none hover:border-tertiary"
@@ -130,7 +137,9 @@ const Booking = () => {
                 />
               </div>
               <div className="w-full md:w-1/2">
-                <label className="font-semibold">Thời gian nhận bàn</label>
+                <label className="font-semibold">
+                  {t("Booking.label_arrivalTime")}
+                </label>
                 <Input
                   type="time"
                   placeholder=""
@@ -145,14 +154,14 @@ const Booking = () => {
               </div>
             </div>
             <div className="!mt-6 text-center font-medium">
-              Hoặc liên hệ trực tiếp qua:{" "}
+              {t("Booking.content_1")}
               <a className="text-tertiary" href="tel:0292 3831 530">
                 0292 3831 530
               </a>
             </div>
             <div className="!mb-3 !mt-4 flex justify-center">
               <button className="btn-booking w-fit rounded-lg bg-tertiary px-3 py-2.5 font-semibold text-primary hover:bg-yellow-600">
-                Đặt bàn ngay
+                {t("Booking.button")}
               </button>
             </div>
           </form>
