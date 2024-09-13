9dc0854aa00f03f11b519ddb772e5f395dffff9e
Truong Toan <toanb2110106@student.ctu.edu.vn>
2024-09-13 21:38:07 +0700

    feat: add best dish page UI

--- src/pages/BestDish.jsx
+++ src/pages/BestDish.jsx
@@ -1,12 +1,34 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { useDynamicTitle, useTopPage } from "../hooks";
 import { useTranslation } from "react-i18next";
+import DishLayout from "../layouts/DishLayout";
+import DishListGrid from "../components/dish/DishListGrid";
 
 const BestDish = () => {
   const { t } = useTranslation();
+
   useDynamicTitle(t("BreadcrumbsAndTitle.best_dishes"));
   useTopPage();
-  return <>BestDish</>;
+
+  const [sortBy, setSortBy] = useState({});
+  const [filterBy, setFilterBy] = useState({});
+  const [selectedCategory, setSelectedCategory] = useState({});
+
+  console.log(sortBy, filterBy, selectedCategory);
+
+  return (
+    <DishLayout
+      title={t("BestDishPage.title")}
+      sort={{ seletedOption: sortBy, setSelectedOption: setSortBy }}
+      filter={{ selectedFilter: filterBy, setSelectedFilter: setFilterBy }}
+      category={{
+        selectedCategory: selectedCategory,
+        setSelectedCategory: setSelectedCategory,
+      }}
+    >
+      <DishListGrid />
+    </DishLayout>
+  );
 };
 
 export default BestDish;
