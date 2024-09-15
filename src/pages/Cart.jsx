a8d72103e1ca81f55f73fbc94b8912409931567b
Truong Toan <toanb2110106@student.ctu.edu.vn>
2024-09-15 15:49:48 +0700

    feat: add cart UI

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,26 @@
-import { useEffect } from "react";
 import { useDynamicTitle, useTopPage } from "../hooks";
 import { useTranslation } from "react-i18next";
+import BodyLayout from "../layouts/BodyLayout";
+import CartList from "../components/cart/CartList";
 
 const CartPage = () => {
   const { t } = useTranslation();
+
   useDynamicTitle(t("BreadcrumbsAndTitle.cart"));
   useTopPage();
-  return <>CartPage</>;
+
+  return (
+    <BodyLayout>
+      <div className="py-6">
+        <p className="text-lg font-bold uppercase text-tertiary">
+          Giỏ hàng của bạn
+        </p>
+        <div className="mt-6">
+          <CartList />
+        </div>
+      </div>
+    </BodyLayout>
+  );
 };
 
 export default CartPage;
