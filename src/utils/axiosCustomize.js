2f5ba9163655798c33e0eed5bf93ee49d245bd9b
Truong Toan <toanb2110106@student.ctu.edu.vn>
2024-10-06 16:09:35 +0700

    fix: fix axios interceptor

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -24,6 +24,7 @@ instance.interceptors.request.use(
   function (config) {
     // Do something before request is sent
     // Config header
+    config.headers["User-Role"] = "customer";
 
     return config;
   },
