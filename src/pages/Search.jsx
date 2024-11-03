f71a2f084068f0d9ad59e7b1029446463c2bd08b
Truong Toan <toanb2110106@student.ctu.edu.vn>
2024-11-03 14:57:34 +0700

    fix: loading when searching

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,7 +8,9 @@ import { toast } from "react-toastify";
 import BodyLayout from "../layouts/BodyLayout";
 import Pagination from "../components/pagination/Pagination";
 import DishCard from "../components/dish/DishCard";
-import { LIMIT_PAGE } from "../constants";
+import { Spin } from "antd";
+
+const LIMIT_PAGE = 15;
 
 const SearchPage = () => {
   const { t } = useTranslation();
@@ -26,7 +28,7 @@ const SearchPage = () => {
 
   const keyword = searchParams.get("keyword");
 
-  const { apiFunction: handleSearch } = useApi(
+  const { apiFunction: handleSearch, loading } = useApi(
     async (keyword, page, limit) => await search(keyword, page, limit),
   );
 
@@ -54,38 +56,44 @@ const SearchPage = () => {
 
   return (
     <BodyLayout>
-      <div className="py-6">
-        {results?.data && results?.data.length > 0 ? (
-          <>
-            <div className="pb-6 text-base font-bold text-primary sr-530:text-lg sm:text-xl">
-              {t("SearchPage.title", {
-                total: results?.totalData,
-                keyword: keyword,
-              })}
-            </div>
-            <div className="space-y-12">
-              <div className="-mx-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-                {results?.data &&
-                  results?.data.length > 0 &&
-                  results?.data.map((dish) => {
-                    return <DishCard key={dish._id} dish={dish} />;
-                  })}
+      {loading ? (
+        <div className="flex items-center justify-center py-8 sr-950:py-20">
+          <Spin />
+        </div>
+      ) : (
+        <div className="py-6">
+          {results?.data && results?.data.length > 0 ? (
+            <>
+              <div className="pb-6 text-base font-bold text-primary sr-530:text-lg sm:text-xl">
+                {t("SearchPage.title", {
+                  total: results?.totalData,
+                  keyword: keyword,
+                })}
               </div>
-              <Pagination
-                totalPages={results?.totalPages}
-                currentPage={currentPage}
-                onChangePage={handleChangePage}
-              />
+              <div className="space-y-12">
+                <div className="-mx-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+                  {results?.data &&
+                    results?.data.length > 0 &&
+                    results?.data.map((dish) => {
+                      return <DishCard key={dish._id} dish={dish} />;
+                    })}
+                </div>
+                <Pagination
+                  totalPages={results?.totalPages}
+                  currentPage={currentPage}
+                  onChangePage={handleChangePage}
+                />
+              </div>
+            </>
+          ) : (
+            <div className="py-2">
+              <div className="rounded-md bg-[#fff3cd] px-4 py-3 text-sm font-medium text-yellow-600">
+                {t("SearchPage.notFound", { keyword: keyword })}
+              </div>
             </div>
-          </>
-        ) : (
-          <div className="py-2">
-            <div className="rounded-md bg-[#fff3cd] px-4 py-3 text-sm font-medium text-yellow-600">
-              {t("SearchPage.notFound", { keyword: keyword })}
-            </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </BodyLayout>
   );
 };
