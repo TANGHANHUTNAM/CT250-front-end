d378c3056c891e674364d6becd8ca9254076604b
Toàn <142390731+tqttoan@users.noreply.github.com>
2024-11-08 00:00:54 +0700

    fix: news title (check)

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -15,7 +15,7 @@ const LIMIT_PAGE = 12;
 const NewsPage = () => {
   const { t } = useTranslation();
 
-  useDynamicTitle(t("BreadcrumbsAndTitle.news")); // news title
+  useDynamicTitle(t("BreadcrumbsAndTitle.all_dishes")); // news title
   useTopPage();
 
   const [news, setNews] = useState({});
